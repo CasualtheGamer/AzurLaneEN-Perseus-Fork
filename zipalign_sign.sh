d65d150a7dbbe00be5e5679d01b645b35e564c58
CasualtheGamer <tqt.casualgamer@gmail.com>
2024-12-11 14:02:42 +0700

    Update zipalign_sign.sh

--- zipalign_sign.sh
+++ zipalign_sign.sh
@@ -3,8 +3,8 @@ PATH=$PATH:$ANDROID_HOME/build-tools/32.0.0/
 for f in build/*.apk; do
     mv $f ${f%.apk}.apk.unsigned
     echo "Zipaligning $f"
-    zipalign -pvf 4 ${f%.apk}.apk.unsigned $f
+    $ANDROID_HOME/build-tools/32.0.0/zipalign -pvf 4 ${f%.apk}.apk.unsigned $f
     rm ${f%.apk}.apk.unsigned
     echo "Signing $f"
-    apksigner sign --key testkey.pk8 --cert testkey.x509.pem $f
+    $ANDROID_HOME/build-tools/32.0.0/apksigner sign --key testkey.pk8 --cert testkey.x509.pem $f
 done
